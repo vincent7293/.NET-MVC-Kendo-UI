caeccb353eca5e61f0a1fd9f3d4b7d68e70ff7aa
vincent7293 <vincent7293@gmail.com>
2017-03-11 00:55:54 +0800

    hi

---
+++ course3.1/Controllers/firstController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace course3.Controllers
+{
+    public class firstController : Controller
+    {
+        public ActionResult Index()
+        {
+			ViewBag.Label = "gg";
+            return View ();
+        }
+    }
+}
