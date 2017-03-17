5a95496ba01b1fade7fb304d850ea3b29a7ed3df
vincent7293 <vincent7293@gmail.com>
2017-03-17 15:16:57 +0800

    1

---
+++ WebApplication1/WebApplication1/Models/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class OrderService
+    {
+        public void InsertOrder()
+        {
+
+        }
+        public void DeleteOrder()
+        {
+
+        }
+        public void UpdateOrder()
+        {
+
+        }
+        public Models.Order GetOrderById(string id)
+        {
+            Models.Order result = new Order();
+            result.CustId = "111";
+            result.CustName = "Vincent";
+            return result;
+        }
+    }
+}
